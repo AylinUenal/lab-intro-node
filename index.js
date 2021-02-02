095f9cea8a25027a4f748b7fb1a1ccea436a1cb5
Aylin Ünal <ayl.uenal@gmail.com>
2021-02-02 20:35:36 +0100

    solve iteration 1-3

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+this.items.push(item);
+this.items.sort(function(a, b) {return a - b});
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) return this.items[pos];
+    else throw new Error('OutOfBounds');
+  }
+
 
   max() {}
 
@@ -15,3 +25,11 @@ class SortedList {
 }
 
 module.exports = SortedList;
+
+const sortedList = new SortedList();
+sortedList.add(4) 
+sortedList.add(3)
+console.log(sortedList.items)  
+//console.log(sortedList.get(5))
+console.log(sortedList.get(0))
+//console.log(sortedList.length)
